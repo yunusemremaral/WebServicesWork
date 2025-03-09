cbf8ee63f3925c0bf8a178ed26e91a82a403e21d
Yunus Emre Maral <108576886+yunusemremaral@users.noreply.github.com>
2025-03-09 20:33:08 +0300

    ..

--- btk-akademi-aspnet-core-web-api-master/basics/HelloWebAPi/Models/ResponseModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HelloWebAPi.Models
-{
-    public class ResponseModel
-    {
-        public int HttpStatus { get; set; }
-        public String Message { get; set; }
-    }
-}
--- btk-akademi-aspnet-core-web-api-master/bookDemo/Models/Book.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace bookDemo.Models
-{
-    public class Book
-    {
-        public int Id { get; set; }
-        public String Title { get; set; }
-        public decimal Price { get; set; }
-    }
-}
